eb39a2a534ef96d16f6f5d0e609b8bfe38703c3c
SungTaoKao <paulkao94@gmail.com>
2017-12-04 12:45:19 -0800

    REST request test

--- ZenithClientSite/src/app/event.ts
+++ ZenithClientSite/src/app/event.ts
@@ -1,16 +1,17 @@
+/*
 export class Event {
     eventId            : number;
-    eventFrom : string;
-    eventTo        : string;
-    username          : string;
-    activityId           : number;
-    activity       : null;
+    eventFrom          : string;
+    eventTo            : string;
+    username           : string;
+    activityId         : number;
+    activity           : null;
     activityCategoryId : number;
-    creationDate  : string;
-    isActive : boolean
+    creationDate       : string;
+    isActive           : boolean
 }
 
-/** 
+*/
 export class Event {
     eventId            : number;
     eventFromDate      : string;
@@ -21,4 +22,3 @@ export class Event {
     activityCategoryId : number;
     activityCategory   : null;
 }
-*/
